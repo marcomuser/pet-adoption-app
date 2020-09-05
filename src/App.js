67d8519ac44aaa938ecbe5f5136bc822385eda31
Marco Muser <marco.muser@posteo.de>
2020-09-05 13:23:00 +0200

    add useDropdown custom hook & modify toolchain

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Pet } from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt me!</h1>
-      <Pet name="Gustavo" animal="Dog" breed="Golden Retriever" />
-      <Pet name="Hans" animal="Cat" breed="Stray" />
-      <Pet name="Ferdinand" animal="Dog" breed="Labradoodle" />
+      <SearchParams />
     </div>
   );
 };
