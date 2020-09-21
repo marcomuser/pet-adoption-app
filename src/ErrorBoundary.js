043d8a01802453f1ebfcff96f106f79d2bae136d
Marco Muser <marco.muser@posteo.de>
2020-09-21 13:13:57 +0200

    add error boundary

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,28 @@
+// mostly code from react docs
+
+import React from "react";
+import { Link } from "@reach/router";
+
+class ErrorBoundary extends React.Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary caught an error", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h1>
+          There was an error with this listing. <Link to="/">Click here</Link>{" "}
+          to back to the home page.
+        </h1>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
